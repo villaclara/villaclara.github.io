f7489dbb52643ac63b4c8f960d98318ad779edc8
villaclara <villaclara1231@gmail.com>
2025-08-12 02:09:25 +0300

    images to projects and some text

--- public/info/projects.js
+++ public/info/projects.js
@@ -1,14 +1,26 @@
 window.projects = [
     {
-        name: "ATA Reborn",
-        description: "",
+        name: "ATA Reborn Windows App",
+        description: "Developed a desktop application for tracking usage time of Windows processes in real-time. Used WPF framework, designed with MVVM pattern. Can be installed from .msix package.",
         tags: [ "WPF", ".NET", "MVVM" ],
-        link: "https://github.com/villaclara/ata-reborn"
+        link: "https://github.com/villaclara/ata-reborn",
+        image: "../public/images/ata_reborn.png",
+        img_album_orientation: true
     },
     {
-        name: "BabyEST",
-        description: "time tracker app",
+        name: "BabyEST Web App",
+        description: "Developed a web app to help parents track their toddler's sleep and eating schedules as they grow. Angular Front-end backed by .NET Minimal Api back-end. Designed UI for phones, installable as PWA application. Light/dark themes available.",
         tags: [ "ASP.NET Core", "Angular", "WebApi", "PWA" ],
-        link: "https://github.com/villaclara/baby-est"
+        link: "https://github.com/villaclara/baby-est",
+        image: "../public/images/baby_est.png",
+        img_album_orientation: false
+    },
+    {
+        name: "Road23 Admin Web App",
+        description: "Developed a web app for my wife small candle shop to manage products/orders. Blazor WASM as front-end, backed by .NET Controller based back-end following Repository pattern. Mobile first UI designed with Boostrap + CSS, installable as PWA application.",
+        tags: [ "ASP.NET Core", "Blazor", "PWA" ],
+        link: "https://github.com/villaclara/r23-admin-app",
+        image: "../public/images/r23_admin_app.png",
+        img_album_orientation: false
     }
 ]
--- src/scripts/fetchdata.js
+++ src/scripts/fetchdata.js
@@ -4,10 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
     window.projects.forEach(product => {
         const div = document.createElement("div");
 
+        // actually the only width are the affected. as changing height has no effect at all. 
+        // is has probably something to do with img tag but for now it look okay
+        const imgW = product.img_album_orientation == true ? "w-40" : "w-22";
+        const imgH = product.img_album_orientation == true ? "h-24" : "h-40";
         div.innerHTML = `
             <a href="${product.link}" target="_blank">
-                <div class="border border-slate-700 p-4 rounded [&:not(:hover)]:group-hover:opacity-50 hover:bg-gray-800 hover:blur-0">
-                <img src="${product.name}"  width=200 height=40 class="flex-shrink-0" />
+                <div class="flex flex-col lg:flex-row space-x-3 border border-slate-900 p-4 rounded-xl [&:not(:hover)]:group-hover:opacity-50
+                 hover:bg-gray-800 hover:blur-0 hover:scale-105 transition duration-200 transform">
+                <img src="${product.image}" class="${imgW} ${imgH} flex-shrink-0" />
                 <div>
                     <h3 class="text-lg font-semibold text-slate-100">${product.name}</h3>
                     <p class="text-slate-400">${product.description}</p>
