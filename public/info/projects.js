a74cb720c86c3d9974d31e2d34efe9f3d6b14e1a
villaclara <villaclara1231@gmail.com>
2025-08-15 02:05:40 +0300

    wip projects page. added command and workspace files

--- public/info/projects.js
+++ public/info/projects.js
@@ -1,22 +1,52 @@
 window.projects = [
     {
+        displayInMain: true,
         name: "ATA Reborn Windows App",
         description: "Built a desktop app that tracks how long Windows processes run in real time. It shows active time for the current session, today’s total, and all-time usage, with a chart for a visual overview. Features a clean or minimal layout plus handy QoL settings.<br/> Used WPF framework, designed with MVVM pattern. App is available as an easy-to-install .msix package for Windows.",
         tags: [ "WPF", ".NET", "MVVM" ],
         link: "https://github.com/villaclara/ata-reborn",
         image: "../public/images/ata_reborn.png",
-        img_album_orientation: true
+        img_album_orientation: true,
     },
     {
+        displayInMain: true,
         name: "BabyEST Web App",
         description: "Developed a web app for parents to track their toddler’s sleep and eating schedules, with options to add/edit entries, filter and view history, and see average times. <span class='font-bold'>Key feature:</span> invite another parent to share and update the child’s records from their own account. <br/> Angular frontend with .NET Minimal Api backend. Designe UI for phones with Bootstrap + CSS, installable as PWA application. Light/dark themes available.",
         tags: [ "ASP.NET Core", "Angular", "WebApi", "PWA" ],
         link: "https://github.com/villaclara/baby-est",
         image: "../public/images/baby_est.png",
+        img_album_orientation: false,
+    },
+    {
+        displayInMain: true,
+        name: "Road23 Admin Web App",
+        description: "Developed a web app for my wife small candle shop to manage products/orders. Features include adding/removing candles, tracking orders, a simple to-do list with full history, and an expense tracker. <br/> Blazor WASM frontend with a .NET Controller-based Web API backend using the Repository pattern. Mobile-first UI built with Bootstrap and CSS, installable as a PWA.",
+        tags: [ "ASP.NET Core", "Blazor", "PWA" ],
+        link: "https://github.com/villaclara/r23-admin-app",
+        image: "../public/images/r23_admin_app.png",
         img_album_orientation: false
     },
     {
-        name: "Road23 Admin Web App",
+        displayInMain: false,
+        name: "Bridges Unity WebGL Game",
+        description: "Developed a web app for my wife small candle shop to manage products/orders. Features include adding/removing candles, tracking orders, a simple to-do list with full history, and an expense tracker. <br/> Blazor WASM frontend with a .NET Controller-based Web API backend using the Repository pattern. Mobile-first UI built with Bootstrap and CSS, installable as a PWA.",
+        tags: [ "ASP.NET Core", "Blazor", "PWA" ],
+        link: "https://github.com/villaclara/r23-admin-app",
+        image: "../public/images/r23_admin_app.png",
+        img_album_orientation: false
+    },
+    {
+        displayInMain: false,
+        name: "Bridges Unity WebGL Game",
+        description: "Developed a web app for my wife small candle shop to manage products/orders. Features include adding/removing candles, tracking orders, a simple to-do list with full history, and an expense tracker. <br/> Blazor WASM frontend with a .NET Controller-based Web API backend using the Repository pattern. Mobile-first UI built with Bootstrap and CSS, installable as a PWA.",
+        tags: [ "ASP.NET Core", "Blazor", "PWA" ],
+        link: "https://github.com/villaclara/r23-admin-app",
+        image: "../public/images/r23_admin_app.png",
+        img_album_orientation: false
+    },
+    {
+        displayInMain: false,
+        name: "Bridges Unity WebGL Game",
         description: "Developed a web app for my wife small candle shop to manage products/orders. Features include adding/removing candles, tracking orders, a simple to-do list with full history, and an expense tracker. <br/> Blazor WASM frontend with a .NET Controller-based Web API backend using the Repository pattern. Mobile-first UI built with Bootstrap and CSS, installable as a PWA.",
         tags: [ "ASP.NET Core", "Blazor", "PWA" ],
         link: "https://github.com/villaclara/r23-admin-app",
