6f88907a66c331c43686aeac5db6fb67f85f0add
villaclara <villaclara1231@gmail.com>
2025-08-22 00:32:47 +0300

    added gooserunner

--- src/scripts/fetchdata.js
+++ src/scripts/fetchdata.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 <div class="flex flex-col lg:flex-row gap-3 md:p-4 py-4 rounded-md md:[&:not(:hover)]:group-hover:opacity-50
                  md:hover:bg-gray-800 md:hover:blur-0 md:hover:scale-105 transition duration-200 transform">
                 <div class="flex items-center">
-                    <img src="${product.image}" class="${imgW} ${imgH} flex-shrink-0" />
+                    <img src="${product.image}" alt="${product.name}" class="${imgW} ${imgH} flex-shrink-0" />
                 </div>
                 <div class="space-y-3">
                     <h3 class="text-base font-semibold text-slate-100">${product.name}</h3>
